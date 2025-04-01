7688fd6891fe4fc09e8b5639b8c14a5a28e55afe
st161399 <st161399@stud.uni-stuttgart.de>
2025-04-01 20:37:43 +0200

    added UntiyBrowser

---
+++ AGV/Assets/SimpleWebBrowser/Demo/SampleJSQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using SimpleWebBrowser;
+using MessageLibrary;
+
+public class SampleJSQueryHandler : MonoBehaviour
+{
+
+    public WebBrowser2D MainBrowser;
+
+    void Start()
+    {
+        MainBrowser = GameObject.Find("Browser2D").GetComponent<WebBrowser2D>();
+        MainBrowser.OnShowDialog += MainBrowser_OnJSQuery;
+    }
+
+    private void MainBrowser_OnJSQuery(string message, string prompt, DialogEventType type)
+    {
+        Debug.Log("Javascript query:" + message);
+        MainBrowser.RespondToJSQuery("My response: OK");
+        MainBrowser.RunJavaScript("document.getElementById('test').innerHTML = 'LOL'");
+        MainBrowser.RunJavaScript("document.getElementsByTagName('body')[0].style.height = '100px'");
+    }
+}
