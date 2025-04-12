e24d5a3fcfebfe1f49805c7068724d1ac24fc3ca
st161399 <st161399@stud.uni-stuttgart.de>
2025-04-12 11:56:07 +0200

    added rotation of extra Parts

--- AGV/Assets/RotateAssembly.cs
+++ AGV/Assets/RotateAssembly.cs
@@ -8,10 +8,12 @@ namespace AGV
         float rotationSpeed;
         public Vector3 centerPosition;
         public Vector3 initialPosition;
+        Transform extraParts;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
         {
+            extraParts = transform.parent.Find("ExtraParts");
         }
 
         // Update is called once per frame
@@ -20,32 +22,41 @@ namespace AGV
             if (Input.GetKey(KeyCode.Keypad4))
             {
                 transform.RotateAround(centerPosition, new Vector3(1, 0, 0), rotationSpeed);
+                extraParts.RotateAround(centerPosition, new Vector3(1, 0, 0), rotationSpeed);
             }
             if (Input.GetKey(KeyCode.Keypad6))
             {
                 transform.RotateAround(centerPosition, new Vector3(-1, 0, 0), rotationSpeed);
+                extraParts.RotateAround(centerPosition, new Vector3(-1, 0, 0), rotationSpeed);
             }
             if (Input.GetKey(KeyCode.Keypad7))
             {
                 transform.RotateAround(centerPosition, new Vector3(0, 1, 0), rotationSpeed);
+                extraParts.RotateAround(centerPosition, new Vector3(0, 1, 0), rotationSpeed);
             }
             if (Input.GetKey(KeyCode.Keypad9))
             {
                 transform.RotateAround(centerPosition, new Vector3(0, -1, 0), rotationSpeed);
+                extraParts.RotateAround(centerPosition, new Vector3(0, -1, 0), rotationSpeed);
             }
             if (Input.GetKey(KeyCode.Keypad8))
             {
                 transform.RotateAround(centerPosition, new Vector3(0, 0, -1), rotationSpeed);
+                extraParts.RotateAround(centerPosition, new Vector3(0, 0, -1), rotationSpeed);
             }
             if (Input.GetKey(KeyCode.Keypad5))
             {
                 transform.RotateAround(centerPosition, new Vector3(0, 0, 1), rotationSpeed);
+                extraParts.RotateAround(centerPosition, new Vector3(0, 0, 1), rotationSpeed);
             }
+
 
             if (Input.GetKey(KeyCode.Keypad0))
             {
                 transform.position = initialPosition;
                 transform.localRotation = Quaternion.identity;
+                extraParts.rotation = Quaternion.identity;
+                extraParts.position = Vector3.zero;
             }
         }
     } }
